e415c018256f28934efe0e61d5251c29fb8727b8
Hammouche Melissa <Melissa.Hammouche@anr.fr>
2026-05-27 20:01:31 +0200

    Ajout booktype

---
+++ src/Form/BookType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Author;
+use App\Entity\Book;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BookType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre')
+            ->add('anneedepublication')
+            ->add('author', EntityType::class, [
+                'class' => Author::class,
+                'choice_label' => 'nom',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Book::class,
+        ]);
+    }
+}
